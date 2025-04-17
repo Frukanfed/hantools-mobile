e0120b736063ae542c9c795b4a40c6402cb9672c
frukanfed <frukansalih@gmail.com>
2025-04-17 21:37:38 +0300

    header

--- Hantools/app/(tabs)/main.tsx
+++ Hantools/app/(tabs)/main.tsx
@@ -1,78 +1,97 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import Header from "@/components/Header";
+import React, { useState } from "react";
+import { View, TextInput, FlatList, Text, StyleSheet } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+const data = [
+  {
+    id: "1",
+    name: "Jane Doe",
+    phone: "1111111111",
+    city: "Istanbul",
+    district: "Beşiktaş",
+  },
+  {
+    id: "2",
+    name: "Arthur Morgan",
+    phone: "0000112233",
+    city: "Blackwater",
+    district: "Santa Fe",
+  },
+  {
+    id: "3",
+    name: "Muster1",
+    phone: "22222222",
+    city: "Istanbul",
+    district: "Beşiktaş",
+  },
+  {
+    id: "4",
+    name: "Müşteri",
+    phone: "222",
+    city: "BAŞAKŞEHİR",
+    district: "BAŞAKŞEHİR",
+  },
+];
 
-export default function HomeScreen() {
+export default function Main() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredData = data.filter(
+    (item) =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      item.phone.includes(searchQuery)
+  );
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.Container}>
+      <Header />
+      <TextInput
+        style={styles.SearchBar}
+        placeholder="Search for a customer..."
+        value={searchQuery}
+        onChangeText={(text) => setSearchQuery(text)}
+      />
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.Card}>
+            <Text style={styles.Name}>{item.name}</Text>
+            <Text>{item.phone}</Text>
+            <Text>
+              {item.city} / {item.district}
+            </Text>
+          </View>
+        )}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
+  Container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "white",
   },
-  stepContainer: {
-    gap: 8,
+  SearchBar: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  Card: {
+    padding: 15,
     marginBottom: 8,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#ddd",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  Name: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
---
+++ Hantools/components/Header.tsx
@@ -0,0 +1,48 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { View, Image, Pressable, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+
+export default function Header() {
+  const isAdmin = useSelector((state: any) => state.admin.isAdmin);
+
+  return (
+    <View style={styles.Container}>
+      <Image
+        style={styles.Logo}
+        source={require("../assets/images/mainlogo.png")}
+      />
+      {isAdmin && (
+        // TODO: ROUTER
+        <Pressable onPress={() => {}}>
+          {({ pressed }) => (
+            <FontAwesome
+              size={25}
+              name="shield"
+              style={[styles.AdminPanelButton, { opacity: pressed ? 0.5 : 1 }]}
+              color={"gray"}
+            />
+          )}
+        </Pressable>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    width: "100%",
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+  },
+  Logo: {
+    width: 110,
+    height: 50,
+    resizeMode: "contain",
+    paddingTop: 30,
+  },
+  AdminPanelButton: {
+    paddingTop: 27,
+  },
+});
